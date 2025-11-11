b2a0a46e4ab1df192ab9d1b029bee311dce1c7a0
Viktoriia Petukhova <viktoriiap0205199@gmail.com>
2025-11-11 16:18:02 +0100

    test: test husky push

---
+++ proyecto-final/frontend/.eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier'
+  ],
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react/prop-types': 'off'
+  },
+  settings: {
+    react: { version: 'detect' }
+  },
+  overrides: [
+    {
+      files: ['src/**/*.{js,jsx,ts,tsx}'],
+      rules: {
+      }
+    }
+  ]
+};
