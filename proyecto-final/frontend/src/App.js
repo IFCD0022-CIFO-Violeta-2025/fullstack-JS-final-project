9c5849b805246b7f6eca991e46653f6fcc5d985c
Viktoriia Petukhova <viktoriiap0205199@gmail.com>
2025-11-11 18:47:29 +0100

    test: test push

--- proyecto-final/frontend/src/App.js
+++ proyecto-final/frontend/src/App.js
@@ -11,13 +11,13 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjs.org
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
-      </header
+      </header>
     </div>
   );
 }
