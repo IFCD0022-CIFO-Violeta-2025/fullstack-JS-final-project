3d9e231cbdf3a73886597cc3b43d617726ac7187
Viktoriia Petukhova <viktoriiap0205199@gmail.com>
2025-11-11 19:19:05 +0100

    test

--- proyecto-final/frontend/src/App.js
+++ proyecto-final/frontend/src/App.js
@@ -1,19 +1,20 @@
 import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
+  //return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p
-        
+        </p>
+        <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer
+          rel="noopener noreferrer"
         >
           Learn React
         </a>
@@ -22,4 +23,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; 
