0f41dfa1fbae1c20d0cd7bf0ff11bad8ffcbcdeb
Viktoriia Petukhova <viktoriiap0205199@gmail.com>
2025-11-12 16:07:04 +0100

    fix: update husky

--- proyecto-final/frontend/src/App.jsx
+++ proyecto-final/frontend/src/App.jsx
@@ -4,9 +4,9 @@ import viteLogo from "/vite.svg"
 import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0) Ъefefe
+  const [count, setCount] = useState(0)
 
-  return (rfrf
+  return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
