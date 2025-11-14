29f8ccd15672089230b32bbef693a53e1ac4ea30
Marc <m.caixasmarin@gmail.com>
2025-11-14 18:08:17 +0100

    fix: importar bootstrap en main.jsx

--- proyecto-final/frontend/src/main.jsx
+++ proyecto-final/frontend/src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css"
 import App from "./App.jsx"
 import { ThemeProvider } from "./contexts/ThemeContext.jsx"
 import { BrowserRouter } from "react-router-dom";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
