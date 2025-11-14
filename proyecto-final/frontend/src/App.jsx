f8461f6745122758b5ada53103f0c0f19e8e1131
Marc <m.caixasmarin@gmail.com>
2025-11-14 16:31:45 +0100

    feat: añadir rutas en app.jsx

--- proyecto-final/frontend/src/App.jsx
+++ proyecto-final/frontend/src/App.jsx
@@ -1,11 +1,24 @@
-
-import "./App.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import EventPage from "./pages/EventPage";
+import CreateEventPage from "./pages/CreateEventPage";
+import ProfilePage from "./pages/ProfilePage";
+import FAQPage from "./pages/FAQPage";
 
 function App() {
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/event/:id" element={<EventPage />} />
+        <Route path="/create" element={<CreateEventPage />} />
+        <Route path="/profile/" element={<ProfilePage />} />
+        <Route path="/faq" element={<FAQPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
