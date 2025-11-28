5124e518fd569945d82fa2e16cd958b58742d5cb
Viktoriia <viktoriiap0205199@gmail.com>
2025-11-28 16:59:19 +0100

    Delete src directory

--- src/components/RequireAuth.jsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react'
-import { AuthContext } from '../contexts/AuthContext'
-import { Navigate, Outlet } from 'react-router-dom'
-
-export default function RequireAuth({ redirectTo = '/login' }) {
-  const { isAuthenticated } = useContext(AuthContext)
-  return isAuthenticated ? <Outlet /> : <Navigate to={redirectTo} />
-}
