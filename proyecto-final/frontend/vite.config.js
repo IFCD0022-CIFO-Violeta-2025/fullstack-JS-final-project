556605b4b6f2a6f1aee62df8a7146a63930bd376
ViktoriiaPet <viktoriiap0205199@gmail.com>
2025-11-28 19:02:17 +0100

    fix: update vite.config

--- proyecto-final/frontend/vite.config.js
+++ proyecto-final/frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'fullstack-JS-final-project'
+  base: '/fullstack-JS-final-project/'
 })
